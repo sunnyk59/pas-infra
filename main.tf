b71cd0897f9c6129de79a382e9870db500698440
sunnyk59 <sunny.aus59@gmail.com>
2025-01-23 16:24:01 +1100

    updated workflow

--- main.tf
+++ main.tf
@@ -1,21 +1,77 @@
-provider "aws" {
-  region = "ap-southeast-2"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true 
 }
 
-terraform {
-  required_version = ">= 1.10.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.41.0" 
-    }
-  }
-  backend "s3" {
-    bucket         = "webpas-terraform-state"
-    key            = "terraform/pas-infra/terraform.tfstate"  
-    region         = "ap-southeast-2"
-    dynamodb_table = "webpas-terraform-state-lock"
-    encrypt        = true
+resource "aws_subnet" "subnet_1" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.0.0/20"
+  availability_zone = "ap-southeast-2a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "subnet_2" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.16.0/20"
+  availability_zone = "ap-southeast-2b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "subnet_3" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.32.0/20"
+  availability_zone = "ap-southeast-2c"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id 
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
   }
 }
- 
+
+resource "aws_route_table_association" "subnet_1_association" {
+  subnet_id      = aws_subnet.subnet_1.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_route_table_association" "subnet_2_association" {
+  subnet_id      = aws_subnet.subnet_2.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_route_table_association" "subnet_3_association" {
+  subnet_id      = aws_subnet.subnet_3.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name    = "pas-eks-cluster"
+  cluster_version = "1.27"
+
+  cluster_endpoint_public_access = true
+
+  vpc_id                   = aws_vpc.main.id
+  subnet_ids               = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id, aws_subnet.subnet_3.id]
+  control_plane_subnet_ids = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id, aws_subnet.subnet_3.id]
+
+  eks_managed_node_groups = {
+    green = {
+      min_size       = 1
+      max_size       = 1
+      desired_size   = 1
+      instance_types = ["t3.medium"]
+    }
+  }
+}
+
